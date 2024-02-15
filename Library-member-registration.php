2a7a0e5d7c9895511de1c32b5f35c3517eaeb63d
shanuka dilshan <dilshan-ct19019@stu.kln.ac.lk>
2024-02-15 23:12:06 +0530

    Add java script part

--- Library-member-registration.php
+++ Library-member-registration.php
@@ -128,6 +128,67 @@ if ($conn->connect_error) {
 
 
 
+<script>
+// Validate email format using JavaScript
+function validateEmail() {
+    var emailInput = document.getElementById('email');
+    var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailPattern.test(emailInput.value)) {
+        alert('Please enter a valid email address.');
+        return false;
+    }
+
+    return true;
+}
+
+// Validate Member ID format using JavaScript
+function validateMemberID() {
+    var memberIDInput = document.getElementById('member_id');
+    var memberIDPattern = /^M[0-9]{3}$/;
+
+    if (!memberIDPattern.test(memberIDInput.value)) {
+        alert('Please enter a valid Member ID (e.g., M001).');
+        return false;
+    }
+
+    return true;
+}
+
+function editMember(member_id, first_name, last_name, birthday, email) {
+    // Encode each parameter to ensure special characters are handled properly
+    var encodedFirstName = encodeURIComponent(first_name);
+    var encodedLastName = encodeURIComponent(last_name);
+    var encodedBirthday = encodeURIComponent(birthday);
+    var encodedEmail = encodeURIComponent(email);
+
+    // Redirect to the form page with member details as URL parameters
+    window.location.href = 'Library-member-registration.php?edit_member_id=' + encodeURIComponent(member_id) +
+        '&first_name=' + encodedFirstName +
+        '&last_name=' + encodedLastName +
+        '&birthday=' + encodedBirthday +
+        '&email=' + encodedEmail;
+}
+
+function deleteMember(member_id) {
+    var confirmDelete = confirm('Are you sure you want to delete this member?');
+
+    console.log("Confirm delete: ", confirmDelete);
+
+    if (confirmDelete) {
+        window.location.href = 'Library-member-registration.php?delete_member_id=' + member_id;
+    }
+}
+
+
+
+</script>
+
+
+
+
+
+
 
 
 </head>
