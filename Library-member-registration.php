e963259be3dd3fb024f58fdbf0148d3799a77cbf
shanuka dilshan <dilshan-ct19019@stu.kln.ac.lk>
2024-02-15 23:04:28 +0530

    Add php part to table

--- Library-member-registration.php
+++ Library-member-registration.php
@@ -58,6 +58,56 @@
             </thead>
             <tbody>
 
+            <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $database = "library_system";
+
+                // Create connection
+                $conn = new mysqli($servername, $username, $password, $database);
+
+                // Check connection
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                $result = $conn->query("SELECT * FROM member");
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["member_id"] . "</td>";
+                        echo "<td>" . $row["first_name"] . "</td>";
+                        echo "<td>" . $row["last_name"] . "</td>";
+                        echo "<td>" . $row["birthday"] . "</td>";
+                        echo "<td>" . $row["email"] . "</td>";
+                        echo "<td>";
+                        echo "<button class='edit' onclick='editMember(\"$row[member_id]\", \"$row[first_name]\", \"$row[last_name]\", \"$row[birthday]\", \"$row[email]\")'>Edit</button>&nbsp&nbsp";
+                        echo "<button class='delete' onclick='deleteMember(\"" . $row['member_id'] . "\")'>Delete</button>";
+                        echo "</td>";
+                        echo "</tr>";
+                    }
+                }
+
+                $conn->close();
+
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
 
 
 
@@ -65,8 +115,3 @@
 
 
 
-
-
-
-</body>
-</html>
