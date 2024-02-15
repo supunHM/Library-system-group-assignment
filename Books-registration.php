2f994c9e3819428548f13ce79701c4958a94f6c8
SharmilaNaushad <148603337+SharmilaNaushad@users.noreply.github.com>
2024-02-16 00:47:51 +0530

    Update Books-registration.php
    books registration form CSS part added

--- Books-registration.php
+++ Books-registration.php
@@ -5,7 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    
+    <style>
+        .card {
+            margin-top: 50px;
+        }
+    </style>
 </head>
 <body>
 
