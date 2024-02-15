ec406e6b1988c0b9c00fa2e74ed6deba4dffa485
SanthushyaaJayathilleka <107042244+SanthushyaaJayathilleka@users.noreply.github.com>
2024-02-15 22:32:07 +0530

    create login form

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
     <title>User login</title>
 </head>
 <body> 
-    
+    <div class="login-container">
+    <h2>Login</h2>
+ 
+    <form action="index.php" method="post">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit" name="login">Login</button>
+    </form>
+    <div class="register-link">
+        <a href="#">Create an account</a>
+    </div>
+</div>
 </body>
 </html>
