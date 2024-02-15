08c02c1d7709ddcc1d64b9df2f9b0eb02de895ad
shanuka dilshan <dilshan-ct19019@stu.kln.ac.lk>
2024-02-15 23:07:34 +0530

    create php part

--- Library-member-registration.php
+++ Library-member-registration.php
@@ -1,3 +1,36 @@
+<?php
+// Start the session at the beginning of the file
+session_start();
+
+// Set error reporting for debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "library_system";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
