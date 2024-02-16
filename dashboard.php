72c0e1b9b9fdd2d862784d9acd982e2d72264631
SanthushyaaJayathilleka <107042244+SanthushyaaJayathilleka@users.noreply.github.com>
2024-02-17 00:08:16 +0530

    added dashboard html

---
+++ dashboard.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Library Management System</title>
+    <link rel="stylesheet" href="style.css">
+
+  </head>
+  <body>
+    <div class="card card-style">
+        <div class="card-body">
+           <center><h1 class="h1_top">Library  Management  System</h1></center>
+        </div>
+
+
+        <div class="row justify-content-center">
+         
+          <div class="col-3">
+            <div class="card border-primary bg-light-blue border-2 mt-3 mx-3 mb-4" style="max-width: 20rem; background: #b2bfdb;">
+            
+              <div class="card text-white custom-dark-blue mt-4 mx-5 mb-5" style="max-width: 19rem;">
+                    <div class="card-body">
+                      <h5 class="card-title"><center>Registration</center></h5>
+         
+                    </div>
+                  </div>
+
+                <div class="card text-white custom-dark-blue mx-5 mb-5" style="max-width: 19rem;">
+                    <div class="card-body">
+                      <h5 class="card-title"><center>Book Category Registration</center></h5>
+                      
+                    </div>
+                  </div>
+
+                  <div class="card text-white custom-dark-blue mx-5 mb-5" style="max-width: 19rem;">
+                    <div class="card-body">
+                      <h5 class="card-title"><center>Member Registration</center></h5>
+                     
+                    </div>
+                  </div>
+
+                  <div class="card text-white custom-dark-blue mx-5 mb-5" style="max-width: 19rem;">
+                    <div class="card-body">
+                      <h5 class="card-title"><center>Book Borrow Details</center></h5>
+                      
+                    </div>
+                  </div>
+
+                  <div class="card text-white custom-dark-blue mx-5 mb-4" style="max-width: 19rem;">
+                    <div class="card-body">
+                      <h5 class="card-title"><center>User List</center></h5>
+                    
+                    </div>
+                  </div>
+                 
+                </div>
+          </div>
+
+          <div class="col-9 p-4">
+            <img src="img3.png" style="width: 67%; padding-left: 15%">
+          </div>
+      </div>
+      
+        
+            
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+
+
